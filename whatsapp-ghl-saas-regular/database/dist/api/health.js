bdd5827d2571d10ff9436abb9685963cfd0cc356
Wahedul Islam <165555801+adnanPBI@users.noreply.github.com>
2026-01-27 16:22:04 +0600

    Delete whatsapp-ghl-saas-regular/database/dist directory
    Signed-off-by: Wahedul Islam <165555801+adnanPBI@users.noreply.github.com>

--- whatsapp-ghl-saas-regular/database/dist/api/health.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = handler;
-function handler(_req, res) {
-    res.status(200).json({ ok: true });
-}
-//# sourceMappingURL=health.js.map
